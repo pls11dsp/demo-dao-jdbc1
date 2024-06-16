8d3afb208ab498c58bacc1f92d99eb45f1bd9ab2
pls11dsp <pls11@dspcontroles.com.br>
2024-06-16 15:18:38 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,10 +10,8 @@ public class Program {
 					
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== Test 1: sellerFindById ===");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
-
 	}
-
 }
