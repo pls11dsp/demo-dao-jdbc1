509f6055f755b286b57c68a6ff7cf3a662ecab5d
pls11dsp <pls11@dspcontroles.com.br>
2024-06-17 05:38:56 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,5 +23,11 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== Test 3: seller FindAll ===");
+		 list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 }
