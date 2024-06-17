9f86e0af6704076f8dc13cc72fbf30d48c8b6cb6
pls11dsp <pls11@dspcontroles.com.br>
2024-06-17 16:41:59 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,21 +13,27 @@ public class Program {
 					
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println("=== Test 1: seller FindById ===");
+		System.out.println("=== Test 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
-		System.out.println("\n=== Test 2: seller FindByDepatment ===");
+		System.out.println("\n=== Test 2: seller findByDepatment ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== Test 3: seller FindAll ===");
+		System.out.println("\n=== Test 3: seller findAll ===");
 		 list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== Test 4: seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new java.util.Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted: New id = " + newSeller.getId());
 	}
 }
+
